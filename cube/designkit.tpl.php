dd82234a296a2bae47ba356a91521a193bb0fadc
Young Hahn <yfhahn@gmail.com>
2009-12-15 17:17:03 -0500

    Implementing default colors.

--- cube/designkit.tpl.php
+++ cube/designkit.tpl.php
@@ -3,8 +3,15 @@
 #page-title ul.links li a.active {
   background-color:<?php print $background ?>;
   }
+
 #tabs li.active a,
 #tabs li a.active {
+  background-color:<?php print $background ?>;
+  }
+
+input.form-submit:hover {
+  border-color:<?php print designkit_colorshift($background, '#000000', .2) ?>;
+  border-bottom-color:<?php print designkit_colorshift($background, '#000000', .4) ?>;
   background-color:<?php print $background ?>;
   }
 </style>
